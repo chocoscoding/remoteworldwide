d2e2bc6f7a70fdff334ea8c17b14333ba8265d9e
chocoscoding <chocoscoding@gmail.com>
2024-10-06 08:20:00 -0500

    added sidebar

---
+++ app/components/navigation/AdminNavbar.tsx
@@ -0,0 +1,103 @@
+"use client"; // Since we are using React state
+
+import { useNavbar } from "@/provider/NavbarContext";
+import Link from "next/link"; // Use Next.js' Link for navigation
+
+const AdminNavbar = () => {
+  const { isOpen, toggleNavbar, closeNavbar } = useNavbar();
+
+  return (
+    <nav className="bg-white border-b border-gray-200 sticky z-20 top-0">
+      <div className="max-w-[1580px] mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          {/* Logo */}
+          <div className="flex items-center">
+            <Link href="/">
+              <p className="text-2xl font-bold">Logo</p>
+            </Link>
+          </div>
+
+          {/* Desktop Menu */}
+          <div className="hidden md:flex items-center space-x-4">
+            <Link href="./jobs">
+              <p className="text-gray-700 hover:text-gray-900">Jobs</p>
+            </Link>
+            <Link href="./companies">
+              <p className="text-gray-700 hover:text-gray-900">Companies</p>
+            </Link>
+            <Link href="./categories">
+              <p className="text-gray-700 hover:text-gray-900">Categories</p>
+            </Link>
+            <Link href="./blogs">
+              <p className="text-gray-700 hover:text-gray-900">Blogs</p>
+            </Link>
+
+            <button className="px-5 py-2 bg-black text-white font-bold">Create Job</button>
+
+            <div className="relative">
+              <img
+                src="/path/to/user-image.jpg"
+                alt="User"
+                className="w-8 h-8 rounded-full cursor-pointer"
+                onClick={toggleNavbar} // Assuming toggleNavbar will handle the modal visibility
+              />
+
+              {isOpen && (
+                <div className="fixed right-2 mt-2 top-16 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-20">
+                  <Link href="/bookmarks">
+                    <p onClick={closeNavbar} className="block px-4 py-3 text-gray-700 hover:bg-gray-100">
+                      Bookmarks
+                    </p>
+                  </Link>
+                  <button
+                    onClick={() => {
+                      closeNavbar();
+                      // Add your logout logic here
+                    }}
+                    className="block w-full text-left px-4 py-3 text-gray-700 hover:bg-gray-100">
+                    Logout
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* Mobile Menu Toggle (Hamburger Icon) */}
+          <div className="md:hidden flex items-center">
+            <button onClick={toggleNavbar} className="text-gray-700 hover:text-gray-900 focus:outline-none">
+              {isOpen ? (
+                <p className="h-6 w-6" aria-hidden="true">
+                  l
+                </p> // Close icon when menu is open
+              ) : (
+                <p className="h-6 w-6" aria-hidden="true">
+                  open
+                </p> // Hamburger icon when menu is closed
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            <Link href="/jobs">
+              <p onClick={closeNavbar} className="block px-3 py-2 rounded-md text-base font-medium text-primary hover:bg-gray-50">
+                Find jobs
+              </p>
+            </Link>
+            <Link href="/companies">
+              <p onClick={closeNavbar} className="block px-3 py-2 rounded-md text-base font-medium text-primary hover:bg-gray-50">
+                Companies
+              </p>
+            </Link>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default AdminNavbar;
---
+++ app/components/navigation/Sidebar.tsx
@@ -0,0 +1,121 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Menu, Home, Briefcase, Building, List, User, LogOut, ChevronDown } from "lucide-react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+const menuItems = [
+  { name: "Home", icon: Home, path: "/", section: "home" },
+  {
+    name: "Company",
+    icon: Building,
+    path: "/companies",
+    section: "companies",
+    subItems: [
+      { label: "Create", path: "/companies/create" },
+      { label: "Companies", path: "/companies" },
+    ],
+  },
+  {
+    name: "Job",
+    icon: Briefcase,
+    path: "/jobs",
+    section: "jobs",
+    subItems: [
+      { label: "Create", path: "/jobs/create" },
+      { label: "Jobs", path: "/jobs" },
+      { label: "Inactive", path: "/jobs/inactive" },
+    ],
+  },
+  {
+    name: "Categories",
+    icon: List,
+    path: "/categories",
+    section: "categories",
+    subItems: [
+      { label: "Create", path: "/categories/create" },
+      { label: "Categories", path: "/categories" },
+    ],
+  },
+  {
+    name: "Blog",
+    icon: List,
+    path: "/blogs",
+    section: "blog",
+    subItems: [
+      { label: "Create Blog", path: "/blogs/create" },
+      { label: "Create Author", path: "/blogs/authors/create" },
+      { label: "Blogs", path: "/blogs" },
+      { label: "Authors", path: "/blogs/authors" },
+    ],
+  },
+];
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [openAccordion, setOpenAccordion] = useState<string>("home");
+  const pathname = usePathname();
+
+  const toggleSidebar = () => setIsOpen(!isOpen);
+  const toggleAccordion = (section: string) => setOpenAccordion(openAccordion === section ? "home" : section);
+
+  useEffect(() => setIsOpen(false), [pathname]);
+
+  const isActive = (route: string) => pathname === "/heroshima" + route;
+
+  return (
+    <div className={`flex flex-col h-screen bg-gray-900 text-white ${isOpen ? "w-64" : "w-16"} transition-width duration-300 sticky top-0`}>
+      <div className={`flex items-center ${isOpen ? "justify-between" : "justify-center"} p-4 bg-gray-900 border-b border-b-white/10`}>
+        {isOpen && <div className="text-lg font-bold">Logo</div>}
+        <button onClick={toggleSidebar} className="focus:outline-none">
+          <Menu className="w-6 h-6" />
+        </button>
+      </div>
+
+      <div className={`flex-1 p-4 space-y-4 flex flex-col gap-4 ${isOpen ? "" : "items-center"}`}>
+        {menuItems.map((item) => (
+          <div key={item.section} className="space-y-1 w-full">
+            <div
+              className={`flex items-center justify-between cursor-pointer ${
+                openAccordion === item.section ? "text-secondary" : "text-gray-300"
+              }`}
+              onClick={() => toggleAccordion(item.section)}>
+              <div className="flex items-center space-x-4">
+                <item.icon className="w-5 h-5" />
+                {isOpen && <span>{item.name}</span>}
+              </div>
+              {isOpen && item.subItems && (
+                <ChevronDown className={`transition-all duration-500 w-5 h-5 ${openAccordion === item.section ? "rotate-180" : ""}`} />
+              )}
+            </div>
+            {item.subItems && (
+              <ul
+                className={`pl-10 space-y-1 overflow-hidden transition-all duration-500 ${
+                  isOpen && openAccordion === item.section ? "max-h-screen" : "max-h-0"
+                }`}>
+                {item.subItems.map((subItem, index) => (
+                  <Link key={index} href={`/heroshima${subItem.path}`}>
+                    <li className="mt-2 mb-2">{subItem.label}</li>
+                  </Link>
+                ))}
+              </ul>
+            )}
+          </div>
+        ))}
+      </div>
+
+      <div className="p-4 bg-gray-900">
+        <div className={`flex items-center space-x-4 ${isActive("/user") ? "text-secondary" : "text-gray-300"}`}>
+          <User className="w-5 h-5" />
+          {isOpen && <span>User</span>}
+        </div>
+        <div className={`flex items-center space-x-4 mt-4 ${isActive("/logout") ? "text-secondary" : "text-gray-300"}`}>
+          <LogOut className="w-5 h-5" />
+          {isOpen && <span>Logout</span>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
