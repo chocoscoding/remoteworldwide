073b1e0fb498f99e2e1a1977bf2d3b835fc3bcfa
chocoscoding <chocoscoding@gmail.com>
2024-11-25 00:16:05 +0100

    enhance job filtering and search functionality; improve UI messages and remove unused layout. Blogs responsiveness

--- app/(pages)/(general)/blogs/[id]/Client.tsx
+++ app/(pages)/(general)/blogs/[id]/Client.tsx
@@ -24,8 +24,8 @@ const BlogPage: FC<{ blog: Blog }> = ({ blog }) => {
   return (
     <div className="w-full min-h-screen">
       <div className="h-fit block">
-        <div className="relative w-full min-h-[30rem] overflow-hidden flex flex-col justify-end">
-          <div className="absolute w-full bg-black h-[30rem] z-0"></div>
+        <div className="relative w-full h-[14rem] md:h-[20rem] lg:h-[24rem] xl:h-[27rem] overflow-hidden flex flex-col justify-end">
+          <div className="absolute w-full bg-black h-full z-0"></div>
           <div className="font-extrabold w-full text-center px-10 tracking-tight text-gray-100 lg:text-5xl sm:text-4xl text-2xl z-10 relative">
             {blog.title}
           </div>
@@ -41,27 +41,28 @@ const BlogPage: FC<{ blog: Blog }> = ({ blog }) => {
             <p className="ml-2 text-gray-300">{blog.author.name}</p>
           </Link>
           <div className="sm:flex items-center justify-center space-x-2 text-sm text-gray-300 z-10 relative mt-2 mb-4">
-            <p className="">
+            <p className="text-center">
               <span className="text-slate-200">Created at:</span> {blog.createdAt}
             </p>
             {blog.createdAt !== blog.updatedAt && (
-              <>
-                <span className="sm:block hidden">•</span>
-                <span className="0">Published {new Date(blog.updatedAt).toLocaleDateString("en-GB")}</span>
-              </>
+              <span className="0">Published {new Date(blog.updatedAt).toLocaleDateString("en-GB")}</span>
             )}
           </div>
-          <Image
-            className="rounded-lg w-screen border absolute opacity-50 top-0"
-            src={blog.coverImage}
-            placeholder="blur"
-            blurDataURL={blog.coverImage}
-            layout="intrinsic"
-            width={1080}
-            height={720}
-            alt={"article cover"}
-            priority
-          />
+          <div className="rounded-lg !w-full !h-full border absolute opacity-50 top-0">
+            <div className="w-full h-full relative">
+              <Image
+                className="!h-full sm:h-auto sm:!w-full object-cover"
+                src={blog.coverImage}
+                placeholder="blur"
+                blurDataURL={blog.coverImage}
+                layout="intrinsic"
+                width={1080}
+                height={720}
+                alt={"article cover"}
+                priority
+              />
+            </div>
+          </div>
         </div>
       </div>
 
