324bd74c6f02eb68f2e8c65036bd93d732874252
chocoscoding <chocoscoding@gmail.com>
2025-01-02 23:43:22 +0100

    remove revalidation export from general page

--- app/(pages)/(general)/page.tsx
+++ app/(pages)/(general)/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { fetchLatestJobs, getAllActiveJobsCount } from "@/libs/query";
 // import { revalidatePath } from "next/cache";
 
-export const revalidate = 3600 * 60 * 24;
+// export const revalidate = 3600 * 60 * 24;
 const getLatestJobs = async () => {
   try {
     const latestJobs = await fetchLatestJobs(10);
