91e2e2cf37d9908420b2add0cc0dfd6360d0c2fa
chocoscoding <chocoscoding@gmail.com>
2026-09-14 14:32:34 +0100

    .

--- app/components/Downtime.tsx
+++ app/components/Downtime.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Downtime = () => {
   return (
-    <div className="w-full static top-0 h-14 text-center text-black bg-lime-500 font-bold">{`
-        We are experiencing a bit of downtime. Kindly bear with us`}</div>
+    <div className="w-full static top-0 py-2 text-center text-black bg-lime-500 font-bold">{`
+        We are experiencing a bit of downtime. Kindly bear with us as we fix it`}</div>
   );
 };
 
